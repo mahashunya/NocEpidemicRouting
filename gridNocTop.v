c00d1edc6ffb234a1f5a0442dce948bc8f63bbfb
mahashunya <43090199+mahashunya@users.noreply.github.com>
2022-03-04 00:43:07 +0530

    Removed unnecessary code and corrected port connections

--- gridNocTop.v
+++ gridNocTop.v
@@ -1,32 +1,25 @@
+`timescale 1ns/1ps
+
 module gridNocTop #(parameter X=4,Y=4) (
     input wire clk,
     input wire rstn
 );
 
-wire i_ready_l[X-1:0][Y-1:0];
-wire i_ready_r[X-1:0][Y-1:0];
-wire i_ready_t[X-1:0][Y-1:0];
-wire i_ready_b[X-1:0][Y-1:0];
-wire o_ready_l[X-1:0][Y-1:0];
-wire o_ready_r[X-1:0][Y-1:0];
-wire o_ready_t[X-1:0][Y-1:0];
-wire o_ready_b[X-1:0][Y-1:0];
-wire i_valid_l[X-1:0][Y-1:0];
-wire i_valid_r[X-1:0][Y-1:0];
-wire i_valid_t[X-1:0][Y-1:0];
-wire i_valid_b[X-1:0][Y-1:0];
-wire o_valid_l[X-1:0][Y-1:0];
-wire o_valid_r[X-1:0][Y-1:0];
-wire o_valid_t[X-1:0][Y-1:0];
-wire o_valid_b[X-1:0][Y-1:0];
-wire [7:0] i_data_l[X-1:0][Y-1:0];
-wire [7:0] i_data_r[X-1:0][Y-1:0];
-wire [7:0] i_data_t[X-1:0][Y-1:0];
-wire [7:0] i_data_b[X-1:0][Y-1:0];
-wire [7:0] o_data_l[X-1:0][Y-1:0];
-wire [7:0] o_data_r[X-1:0][Y-1:0];
-wire [7:0] o_data_t[X-1:0][Y-1:0];
-wire [7:0] o_data_b[X-1:0][Y-1:0];
+wire ready_l[X-1:0][Y-1:0];
+wire ready_r[X-1:0][Y-1:0];
+wire ready_t[X-1:0][Y-1:0];
+wire ready_b[X-1:0][Y-1:0];
+
+wire valid_l[X-1:0][Y-1:0];
+wire valid_r[X-1:0][Y-1:0];
+wire valid_t[X-1:0][Y-1:0];
+wire valid_b[X-1:0][Y-1:0];
+
+wire [7:0] data_l[X-1:0][Y-1:0];
+wire [7:0] data_r[X-1:0][Y-1:0];
+wire [7:0] data_t[X-1:0][Y-1:0];
+wire [7:0] data_b[X-1:0][Y-1:0];
+
 
 
 generate 
@@ -39,29 +32,29 @@ generate
                .clk(clk), 
                .rstn(rstn),
                .i_ready_l(1'b0),
-               .i_ready_r(o_ready_l[x+1][y]),
-               .i_ready_t(o_ready_b[x][y+1]),
+               .i_ready_r(ready_l[x+1][y]),
+               .i_ready_t(ready_b[x][y+1]),
                .i_ready_b(1'b0),
                .i_valid_l(1'b0),
-               .i_valid_r(o_valid_l[x+1][y]),
-               .i_valid_t(o_valid_b[x][y+1]),
+               .i_valid_r(valid_l[x+1][y]),
+               .i_valid_t(valid_b[x][y+1]),
                .i_valid_b(1'b0),
-               .o_ready_l(1'b0),
-               .o_ready_r(i_ready_l[x+1][y]),
-               .o_ready_t(i_ready_b[x][y+1]),
-               .o_ready_b(1'b0),
-               .o_valid_l(1'b0),
-               .o_valid_r(i_valid_l[x+1][y]),
-               .o_valid_t(i_valid_b[x][y+1]),
-               .o_valid_b(1'b0),
+               .o_ready_l(),
+               .o_ready_r(ready_l[x+1][y]),
+               .o_ready_t(ready_b[x][y+1]),
+               .o_ready_b(),
+               .o_valid_l(),
+               .o_valid_r(valid_l[x+1][y]),
+               .o_valid_t(valid_b[x][y+1]),
+               .o_valid_b(),
                .i_data_l(8'b00000000),
-               .i_data_r(o_data_l[x+1][y]),
-               .i_data_t(o_data_b[x][y+1]),
+               .i_data_r(data_l[x+1][y]),
+               .i_data_t(data_b[x][y+1]),
                .i_data_b(8'b00000000),
-               .o_data_l(8'b00000000),
-               .o_data_r(i_data_l[x+1][y]),
-               .o_data_t(i_data_b[x][y+1]),
-               .o_data_b(8'b00000000)
+               .o_data_l(),
+               .o_data_r(data_l[x+1][y]),
+               .o_data_t(data_b[x][y+1]),
+               .o_data_b()
           );
       end
 
@@ -70,30 +63,30 @@ generate
           node n_instance(
                .clk(clk), 
                .rstn(rstn),
-               .i_ready_l(o_ready_r[x-1][y]),
+               .i_ready_l(ready_r[x-1][y]),
                .i_ready_r(1'b0),
                .i_ready_t(1'b0),
-               .i_ready_b(o_ready_t[x][y-1]),
-               .i_valid_l(o_valid_r[x-1][y]),
+               .i_ready_b(ready_t[x][y-1]),
+               .i_valid_l(valid_r[x-1][y]),
                .i_valid_r(1'b0),
                .i_valid_t(1'b0),
-               .i_valid_b(o_valid_t[x][y-1]),
-               .o_ready_l(i_ready_r[x-1][y]),
-               .o_ready_r(1'b0),
-               .o_ready_t(1'b0),
-               .o_ready_b(i_ready_t[x][y-1]),
-               .o_valid_l(i_valid_r[x-1][y]),
-               .o_valid_r(1'b0),
-               .o_valid_t(1'b0),
-               .o_valid_b(i_valid_t[x][y-1]),
-               .i_data_l(o_data_r[x-1][y]),
+               .i_valid_b(valid_t[x][y-1]),
+               .o_ready_l(ready_r[x-1][y]),
+               .o_ready_r(),
+               .o_ready_t(),
+               .o_ready_b(ready_t[x][y-1]),
+               .o_valid_l(valid_r[x-1][y]),
+               .o_valid_r(),
+               .o_valid_t(),
+               .o_valid_b(valid_t[x][y-1]),
+               .i_data_l(data_r[x-1][y]),
                .i_data_r(8'b00000000),
                .i_data_t(8'b00000000),
-               .i_data_b(o_data_t[x][y-1]),
-               .o_data_l(i_data_r[x-1][y]),
-               .o_data_r(8'b00000000),
-               .o_data_t(8'b00000000),
-               .o_data_b(i_data_t[x][y-1])
+               .i_data_b(data_t[x][y-1]),
+               .o_data_l(data_r[x-1][y]),
+               .o_data_r(),
+               .o_data_t(),
+               .o_data_b(data_t[x][y-1])
                );
       end
 
@@ -102,30 +95,30 @@ generate
            node n_instance(
                .clk(clk), 
                .rstn(rstn),
-               .i_ready_l(o_ready_r[x-1][y]),
-               .i_ready_r(o_ready_l[x+1][y]),
-               .i_ready_t(o_ready_b[x][y+1]),
+               .i_ready_l(ready_r[x-1][y]),
+               .i_ready_r(ready_l[x+1][y]),
+               .i_ready_t(ready_b[x][y+1]),
                .i_ready_b(1'b0),
-               .i_valid_l(o_valid_r[x-1][y]),
-               .i_valid_r(o_valid_l[x+1][y]),
-               .i_valid_t(o_valid_b[x][y+1]),
+               .i_valid_l(valid_r[x-1][y]),
+               .i_valid_r(valid_l[x+1][y]),
+               .i_valid_t(valid_b[x][y+1]),
                .i_valid_b(1'b0),
-               .o_ready_l(i_ready_r[x-1][y]),
-               .o_ready_r(i_ready_l[x+1][y]),
-               .o_ready_t(i_ready_b[x][y+1]),
-               .o_ready_b(1'b0),
-               .o_valid_l(i_valid_r[x-1][y]),
-               .o_valid_r(i_valid_l[x+1][y]),
-               .o_valid_t(i_valid_b[x][y+1]),
-               .o_valid_b(1'b0),
-               .i_data_l(o_data_r[x-1][y]),
-               .i_data_r(o_data_l[x+1][y]),
-               .i_data_t(o_data_b[x][y+1]),
+               .o_ready_l(ready_r[x-1][y]),
+               .o_ready_r(ready_l[x+1][y]),
+               .o_ready_t(ready_b[x][y+1]),
+               .o_ready_b(),
+               .o_valid_l(valid_r[x-1][y]),
+               .o_valid_r(valid_l[x+1][y]),
+               .o_valid_t(valid_b[x][y+1]),
+               .o_valid_b(),
+               .i_data_l(data_r[x-1][y]),
+               .i_data_r(data_l[x+1][y]),
+               .i_data_t(data_b[x][y+1]),
                .i_data_b(8'b00000000),
-               .o_data_l(i_data_r[x-1][y]),
-               .o_data_r(i_data_l[x+1][y]),
-               .o_data_t(i_data_b[x][y+1]),
-               .o_data_b(8'b00000000)
+               .o_data_l(data_r[x-1][y]),
+               .o_data_r(data_l[x+1][y]),
+               .o_data_t(data_b[x][y+1]),
+               .o_data_b()
           );
       end
 
@@ -135,29 +128,29 @@ generate
                .clk(clk), 
                .rstn(rstn),
                .i_ready_l(1'b0),
-               .i_ready_r(o_ready_l[x+1][y]),
-               .i_ready_t(o_ready_b[x][y+1]),
-               .i_ready_b(o_ready_t[x][y-1]),
+               .i_ready_r(ready_l[x+1][y]),
+               .i_ready_t(ready_b[x][y+1]),
+               .i_ready_b(ready_t[x][y-1]),
                .i_valid_l(1'b0),
-               .i_valid_r(o_valid_l[x+1][y]),
-               .i_valid_t(o_valid_b[x][y+1]),
-               .i_valid_b(o_valid_t[x][y-1]),
-               .o_ready_l(1'b0),
-               .o_ready_r(i_ready_l[x+1][y]),
-               .o_ready_t(i_ready_b[x][y+1]),
-               .o_ready_b(i_ready_t[x][y-1]),
-               .o_valid_l(1'b0),
-               .o_valid_r(i_valid_l[x+1][y]),
-               .o_valid_t(i_valid_b[x][y+1]),
-               .o_valid_b(i_valid_t[x][y-1]),
+               .i_valid_r(valid_l[x+1][y]),
+               .i_valid_t(valid_b[x][y+1]),
+               .i_valid_b(valid_t[x][y-1]),
+               .o_ready_l(),
+               .o_ready_r(ready_l[x+1][y]),
+               .o_ready_t(ready_b[x][y+1]),
+               .o_ready_b(ready_t[x][y-1]),
+               .o_valid_l(),
+               .o_valid_r(valid_l[x+1][y]),
+               .o_valid_t(valid_b[x][y+1]),
+               .o_valid_b(valid_t[x][y-1]),
                .i_data_l(8'b00000000),
-               .i_data_r(o_data_l[x+1][y]),
-               .i_data_t(o_data_b[x][y+1]),
-               .i_data_b(o_data_t[x][y-1]),
-               .o_data_l(8'b00000000),
-               .o_data_r(i_data_l[x+1][y]),
-               .o_data_t(i_data_b[x][y+1]),
-               .o_data_b(i_data_t[x][y-1])
+               .i_data_r(data_l[x+1][y]),
+               .i_data_t(data_b[x][y+1]),
+               .i_data_b(data_t[x][y-1]),
+               .o_data_l(),
+               .o_data_r(data_l[x+1][y]),
+               .o_data_t(data_b[x][y+1]),
+               .o_data_b(data_t[x][y-1])
           );
       end
 
@@ -166,30 +159,30 @@ generate
            node n_instance(
                .clk(clk), 
                .rstn(rstn),
-               .i_ready_l(o_ready_r[x-1][y]),
-               .i_ready_r(o_ready_l[x+1][y]),
+               .i_ready_l(ready_r[x-1][y]),
+               .i_ready_r(ready_l[x+1][y]),
                .i_ready_t(1'b0),
-               .i_ready_b(o_ready_t[x][y-1]),
-               .i_valid_l(o_valid_r[x-1][y]),
-               .i_valid_r(o_valid_l[x+1][y]),
+               .i_ready_b(ready_t[x][y-1]),
+               .i_valid_l(valid_r[x-1][y]),
+               .i_valid_r(valid_l[x+1][y]),
                .i_valid_t(1'b0),
-               .i_valid_b(o_valid_t[x][y-1]),
-               .o_ready_l(i_ready_r[x-1][y]),
-               .o_ready_r(i_ready_l[x+1][y]),
-               .o_ready_t(1'b0),
-               .o_ready_b(i_ready_t[x][y-1]),
-               .o_valid_l(i_valid_r[x-1][y]),
-               .o_valid_r(i_valid_l[x+1][y]),
-               .o_valid_t(1'b0),
-               .o_valid_b(i_valid_t[x][y-1]),
-               .i_data_l(o_data_r[x-1][y]),
-               .i_data_r(o_data_l[x+1][y]),
+               .i_valid_b(valid_t[x][y-1]),
+               .o_ready_l(ready_r[x-1][y]),
+               .o_ready_r(ready_l[x+1][y]),
+               .o_ready_t(),
+               .o_ready_b(ready_t[x][y-1]),
+               .o_valid_l(valid_r[x-1][y]),
+               .o_valid_r(valid_l[x+1][y]),
+               .o_valid_t(),
+               .o_valid_b(valid_t[x][y-1]),
+               .i_data_l(data_r[x-1][y]),
+               .i_data_r(data_l[x+1][y]),
                .i_data_t(8'b00000000),
-               .i_data_b(o_data_t[x][y-1]),
-               .o_data_l(i_data_r[x-1][y]),
-               .o_data_r(i_data_l[x+1][y]),
-               .o_data_t(8'b00000000),
-               .o_data_b(i_data_t[x][y-1])
+               .i_data_b(data_t[x][y-1]),
+               .o_data_l(data_r[x-1][y]),
+               .o_data_r(data_l[x+1][y]),
+               .o_data_t(),
+               .o_data_b(data_t[x][y-1])
           );
       end
 
@@ -198,62 +191,126 @@ generate
            node n_instance(
                .clk(clk), 
                .rstn(rstn),
-               .i_ready_l(o_ready_r[x-1][y]),
+               .i_ready_l(ready_r[x-1][y]),
                .i_ready_r(1'b0),
-               .i_ready_t(o_ready_b[x][y+1]),
-               .i_ready_b(o_ready_t[x][y-1]),
-               .i_valid_l(o_valid_r[x-1][y]),
+               .i_ready_t(ready_b[x][y+1]),
+               .i_ready_b(ready_t[x][y-1]),
+               .i_valid_l(valid_r[x-1][y]),
                .i_valid_r(1'b0),
-               .i_valid_t(o_valid_b[x][y+1]),
-               .i_valid_b(o_valid_t[x][y-1]),
-               .o_ready_l(i_ready_r[x-1][y]),
-               .o_ready_r(1'b0),
-               .o_ready_t(i_ready_b[x][y+1]),
-               .o_ready_b(i_ready_t[x][y-1]),
-               .o_valid_l(i_valid_r[x-1][y]),
-               .o_valid_r(1'b0),
-               .o_valid_t(i_valid_b[x][y+1]),
-               .o_valid_b(i_valid_t[x][y-1]),
-               .i_data_l(o_data_r[x-1][y]),
+               .i_valid_t(valid_b[x][y+1]),
+               .i_valid_b(valid_t[x][y-1]),
+               .o_ready_l(ready_r[x-1][y]),
+               .o_ready_r(),
+               .o_ready_t(ready_b[x][y+1]),
+               .o_ready_b(ready_t[x][y-1]),
+               .o_valid_l(valid_r[x-1][y]),
+               .o_valid_r(),
+               .o_valid_t(valid_b[x][y+1]),
+               .o_valid_b(valid_t[x][y-1]),
+               .i_data_l(data_r[x-1][y]),
                .i_data_r(8'b00000000),
-               .i_data_t(o_data_b[x][y+1]),
-               .i_data_b(o_data_t[x][y-1]),
-               .o_data_l(i_data_r[x-1][y]),
-               .o_data_r(8'b00000000),
-               .o_data_t(i_data_b[x][y+1]),
-               .o_data_b(i_data_t[x][y-1])
+               .i_data_t(data_b[x][y+1]),
+               .i_data_b(data_t[x][y-1]),
+               .o_data_l(data_r[x-1][y]),
+               .o_data_r(),
+               .o_data_t(data_b[x][y+1]),
+               .o_data_b(data_t[x][y-1])
           );
       end
       
+      else if(x==0 & y==Y-1)
+      begin
+          node n_instance(
+               .clk(clk), 
+               .rstn(rstn),
+               .i_ready_l(1'b0),
+               .i_ready_r(ready_l[x+1][y]),
+               .i_ready_t(1'b0),
+               .i_ready_b(ready_t[x][y-1]),
+               .i_valid_l(1'b0),
+               .i_valid_r(valid_l[x+1][y]),
+               .i_valid_t(1'b0),
+               .i_valid_b(valid_t[x][y-1]),
+               .o_ready_l(),
+               .o_ready_r(ready_l[x+1][y]),
+               .o_ready_t(),
+               .o_ready_b(ready_t[x][y-1]),
+               .o_valid_l(),
+               .o_valid_r(valid_l[x+1][y]),
+               .o_valid_t(),
+               .o_valid_b(valid_t[x][y-1]),
+               .i_data_l(8'b00000000),
+               .i_data_r(data_l[x+1][y]),
+               .i_data_t(8'b00000000),
+               .i_data_b(data_t[x][y-1]),
+               .o_data_l(),
+               .o_data_r(data_l[x+1][y]),
+               .o_data_t(),
+               .o_data_b(data_t[x][y-1])
+          ); 
+      end
+
+      else if(x==X-1 & y==0)
+      begin
+          node n_instance(
+               .clk(clk), 
+               .rstn(rstn),
+               .i_ready_l(ready_r[x-1][y]),
+               .i_ready_r(1'b0),
+               .i_ready_t(ready_b[x][y+1]),
+               .i_ready_b(1'b0),
+               .i_valid_l(valid_r[x-1][y]),
+               .i_valid_r(1'b0),
+               .i_valid_t(valid_b[x][y+1]),
+               .i_valid_b(1'b0),
+               .o_ready_l(ready_r[x-1][y]),
+               .o_ready_r(),
+               .o_ready_t(ready_b[x][y+1]),
+               .o_ready_b(),
+               .o_valid_l(valid_r[x-1][y]),
+               .o_valid_r(),
+               .o_valid_t(valid_b[x][y+1]),
+               .o_valid_b(),
+               .i_data_l(data_r[x-1][y]),
+               .i_data_r(8'b00000000),
+               .i_data_t(data_b[x][y+1]),
+               .i_data_b(8'b00000000),
+               .o_data_l(data_r[x-1][y]),
+               .o_data_r(),
+               .o_data_t(data_b[x][y+1]),
+               .o_data_b()
+          ); 
+      end
+
       else
       begin
            node n_instance(
                .clk(clk), 
                .rstn(rstn),
-               .i_ready_l(o_ready_r[x-1][y]),
-               .i_ready_r(o_ready_l[x+1][y]),
-               .i_ready_t(o_ready_b[x][y+1]),
-               .i_ready_b(o_ready_t[x][y-1]),
-               .i_valid_l(o_valid_r[x-1][y]),
-               .i_valid_r(o_valid_l[x+1][y]),
-               .i_valid_t(o_valid_b[x][y+1]),
-               .i_valid_b(o_valid_t[x][y-1]),
-               .o_ready_l(i_ready_r[x-1][y]),
-               .o_ready_r(i_ready_l[x+1][y]),
-               .o_ready_t(i_ready_b[x][y+1]),
-               .o_ready_b(i_ready_t[x][y-1]),
-               .o_valid_l(i_valid_r[x-1][y]),
-               .o_valid_r(i_valid_l[x+1][y]),
-               .o_valid_t(i_valid_b[x][y+1]),
-               .o_valid_b(i_valid_t[x][y-1]),
-               .i_data_l(o_data_r[x-1][y]),
-               .i_data_r(o_data_l[x+1][y]),
-               .i_data_t(o_data_b[x][y+1]),
-               .i_data_b(o_data_t[x][y-1]),
-               .o_data_l(i_data_r[x-1][y]),
-               .o_data_r(i_data_l[x+1][y]),
-               .o_data_t(i_data_b[x][y+1]),
-               .o_data_b(i_data_t[x][y-1])
+               .i_ready_l(ready_r[x-1][y]),
+               .i_ready_r(ready_l[x+1][y]),
+               .i_ready_t(ready_b[x][y+1]),
+               .i_ready_b(ready_t[x][y-1]),
+               .i_valid_l(valid_r[x-1][y]),
+               .i_valid_r(valid_l[x+1][y]),
+               .i_valid_t(valid_b[x][y+1]),
+               .i_valid_b(valid_t[x][y-1]),
+               .o_ready_l(ready_r[x-1][y]),
+               .o_ready_r(ready_l[x+1][y]),
+               .o_ready_t(ready_b[x][y+1]),
+               .o_ready_b(ready_t[x][y-1]),
+               .o_valid_l(valid_r[x-1][y]),
+               .o_valid_r(valid_l[x+1][y]),
+               .o_valid_t(valid_b[x][y+1]),
+               .o_valid_b(valid_t[x][y-1]),
+               .i_data_l(data_r[x-1][y]),
+               .i_data_r(data_l[x+1][y]),
+               .i_data_t(data_b[x][y+1]),
+               .i_data_b(data_t[x][y-1]),
+               .o_data_l(data_r[x-1][y]),
+               .o_data_r(data_l[x+1][y]),
+               .o_data_t(data_b[x][y+1]),
+               .o_data_b(data_t[x][y-1])
           ); 
       end
      end
